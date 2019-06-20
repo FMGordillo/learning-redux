14255f0eb3768f512d02dff0adb6d7d29203f2c8
FMGordillo <facundomgordillo@gmail.com>
2019-06-19 23:45:41 -0300

    Stylin' and stuff

--- 03-redux-boilerplate/app.js
+++ 03-redux-boilerplate/app.js
@@ -1,5 +1,14 @@
+/**
+|--------------------------------------------------
+| 1) Init the store
+| 2) Create generic function to generate UI for each change
+| 3) Create action creators and use them
+|--------------------------------------------------
+*/
+
 import { createStore } from "redux";
 
+// 1)
 const initialState = {
   recipes: [{ name: "Omelette" }],
   ingredients: [
@@ -16,22 +25,36 @@ const initialState = {
   ]
 };
 
+// 1)
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_RECIPE":
-      break;
+      return Object.assign({}, state, {
+        recipes: state.recipes.concat({ name: action.name })
+      });
+    case "ADD_INGREDIENT":
+      return Object.assign({}, state, {
+        ingredients: state.ingredients.concat({
+          recipe: action.recipe,
+          name: action.name,
+          quantity: action.quantity
+        })
+      });
+    case "REMOVE_INGREDIENT":
+      return state;
     case "REMOVE_RECIPE":
-      break;
+      return state;
     default:
       return state;
   }
 };
 
+// 1)
 const store = createStore(reducer, initialState);
 window.store = store;
 
 /**
- * @author FMGordillo <facundomgordillo@gmail.com>
+ * 2)
  * Magic without JQuery, sry.
  */
 function updateUI() {
@@ -66,7 +89,30 @@ function updateUI() {
   bodyEl.appendChild(container);
 }
 
-// First update
+// 2) first update
 updateUI();
-
 store.subscribe(updateUI);
+
+// 3)
+const addRecipe = name => ({
+  type: "ADD_RECIPE",
+  name
+});
+const addIngredient = (name, quantity, recipe) => ({
+  type: "ADD_INGREDIENT",
+  recipe,
+  name,
+  quantity
+});
+
+// 3)
+document.querySelector("#form_new_recipe").addEventListener("submit", e => {
+  e.preventDefault();
+  const { value } = e.target[0];
+  store.dispatch(addRecipe(value));
+});
+document.querySelector("#form_new_ingredient").addEventListener("submit", e => {
+  e.preventDefault();
+  const [{ value: name }, { value: quantity }, { value: recipe }] = e.target;
+  store.dispatch(addIngredient(name, quantity, recipe));
+});
